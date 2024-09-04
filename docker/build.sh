afae1c775a64c3618ade7e90173731000022c187
Oguz Ozturk <oguzkaganozt@gmail.com>
2024-09-04 20:31:53 +0300

    add multi platform support
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- docker/build.sh
+++ docker/build.sh
@@ -6,13 +6,11 @@ set -e
 print_help() {
     echo "Usage: build.sh [OPTIONS]"
     echo "Options:"
-    echo "  --help          Display this help message"
-    echo "  -h              Display this help message"
-    echo "  --no-cuda       Disable CUDA support"
-    echo "  --platform      Specify the platform (default: current platform)"
-    echo "  --devel-only    Build devel image only"
-    echo ""
-    echo "Note: The --platform option should be one of 'linux/amd64' or 'linux/arm64'."
+    echo "  --help            Display this help message"
+    echo "  -h                Display this help message"
+    echo "  --single-machine  Build the simulator and planning-control images for this platform"
+    echo "Note: By default planning-control built for arm64 and simulator built for amd64"
+    echo "      If you want to build both images for the amd64 platform use --single-machine"
 }
 
 SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
@@ -26,9 +24,8 @@ parse_arguments() {
             print_help
             exit 1
             ;;
-        --platform)
-            option_platform="$2"
-            shift
+        --single-machine)
+            option_single_machine="true"
             ;;
         *)
             echo "Unknown option: $1"
@@ -40,73 +37,41 @@ parse_arguments() {
     done
 }
 
-# Set build options
-set_build_options() {
-    if [ "$option_devel_only" = "true" ]; then
-        targets=("devel")
-    else
-        targets=()
-    fi
-}
-
-# Set platform
-set_platform() {
-    if [ -n "$option_platform" ]; then
-        platform="$option_platform"
-    else
-        platform="linux/amd64"
-        if [ "$(uname -m)" = "aarch64" ]; then
-            platform="linux/arm64"
-        fi
-    fi
-}
-
-# Set arch lib dir
-set_arch_lib_dir() {
-    if [ "$platform" = "linux/arm64" ]; then
-        lib_dir="aarch64"
-    elif [ "$platform" = "linux/amd64" ]; then
-        lib_dir="x86_64"
-    else
-        echo "Unsupported platform: $platform"
-        exit 1
-    fi
-}
-
-# Load env
-load_env() {
-    source "$WORKSPACE_ROOT/amd64.env"
-    if [ "$platform" = "linux/arm64" ]; then
-        source "$WORKSPACE_ROOT/arm64.env"
-    fi
-}
-
 # Build images
 build_images() {
     # https://github.com/docker/buildx/issues/484
     export BUILDKIT_STEP_LOG_MAX_SIZE=10000000
 
-    echo "Building images for platform: $platform"
-    echo "ROS distro: $rosdistro"
-    echo "Base image: $base_image"
-    echo "Setup args: $setup_args"
-    echo "Lib dir: $lib_dir"
-    echo "Image name suffix: $image_name_suffix"
-    echo "Targets: ${targets[*]}"
-
     set -x
-    # Build runtime images
-    docker buildx bake --load --progress=plain -f "$SCRIPT_DIR/docker-bake.hcl" \
-        --set "*.context=$WORKSPACE_ROOT" \
-        --set "*.ssh=default" \
-        --set "*.platform=$platform" \
-        --set "*.args.PLATFORM=$platform" \
-        --set "*.args.ROS_DISTRO=$rosdistro" \
-        --set "*.args.BASE_IMAGE=$base_image" \
-        --set "*.args.LIB_DIR=$lib_dir" \
-        --set "planning-control.tags=ghcr.io/autowarefoundation/openadkit_demo.autoware:ces-planning-before-$lib_dir" \
-        --set "simulator.tags=ghcr.io/autowarefoundation/openadkit_demo.autoware:ces-simulator-$lib_dir" \
-        simulator planning-control
+    # Build images
+    if [ -n "$option_single_machine" ]; then
+        echo "Building images for platform: amd64(x86_64)"
+        docker buildx bake --load --progress=plain -f "$SCRIPT_DIR/docker-bake.hcl" \
+            --set "*.context=$WORKSPACE_ROOT" \
+            --set "*.ssh=default" \
+            --set "*.platform=linux/amd64" \
+            --set "*.args.ARCH=amd64" \
+            --set "planning-control.tags=ghcr.io/autowarefoundation/openadkit_demo.autoware:ces-planning-before-amd64" \
+            --set "simulator.tags=ghcr.io/autowarefoundation/openadkit_demo.autoware:ces-simulator-amd64" \
+            simulator planning-control
+    else
+        echo "Building planning-control for platform: linux/arm64 and simulator for platform: linux/amd64"
+        docker buildx bake --load --progress=plain -f "$SCRIPT_DIR/docker-bake.hcl" \
+            --set "*.context=$WORKSPACE_ROOT" \
+            --set "*.ssh=default" \
+            --set "*.platform=linux/arm64" \
+            --set "*.args.ARCH=arm64" \
+            --set "planning-control.tags=ghcr.io/autowarefoundation/openadkit_demo.autoware:ces-planning-before-arm64" \
+            planning-control
+
+        docker buildx bake --load --progress=plain -f "$SCRIPT_DIR/docker-bake.hcl" \
+            --set "*.context=$WORKSPACE_ROOT" \
+            --set "*.ssh=default" \
+            --set "*.platform=linux/amd64" \
+            --set "*.args.ARCH=amd64" \
+            --set "simulator.tags=ghcr.io/autowarefoundation/openadkit_demo.autoware:ces-simulator-amd64" \
+            simulator
+    fi
     set +x
 }
 
@@ -117,9 +82,5 @@ remove_dangling_images() {
 
 # Main script execution
 parse_arguments "$@"
-set_build_options
-set_platform
-set_arch_lib_dir
-load_env
 build_images
 remove_dangling_images
