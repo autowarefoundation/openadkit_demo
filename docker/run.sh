58a75e46f51cc880e88a17823cff7e01b59c1c1d
Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>
2024-12-07 00:24:57 +0900

    update docker compose script
    Signed-off-by: Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>

--- docker/run.sh
+++ docker/run.sh
@@ -6,4 +6,27 @@ xhost +
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Use the script directory to make the docker-compose.yml path relative
-docker compose -f "$SCRIPT_DIR/docker-compose.yml" up
+docker compose -f "$SCRIPT_DIR/docker-compose-before.yml" up &
+
+sleep 100
+
+echo "terminating containers"
+
+docker container stop $(docker ps -aq)
+
+sleep 3
+
+echo "terminated containers"
+
+
+docker compose -f "$SCRIPT_DIR/docker-compose-after.yml" up &
+
+sleep 100
+
+echo "terminating containers"
+
+docker container stop $(docker ps -aq)
+
+sleep 3
+
+echo "terminated containers"
