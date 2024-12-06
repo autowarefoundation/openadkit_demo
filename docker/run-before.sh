673fcaf6e9e062dccb2beb5c196957526f112834
Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>
2024-12-07 00:29:34 +0900

    update run script
    Signed-off-by: Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>

--- docker/run-before.sh
+++ docker/run-before.sh
@@ -6,5 +6,14 @@ xhost +
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Use the script directory to make the docker-compose.yml path relative
-$SCRIPT_DIR/run-before.sh
-$SCRIPT_DIR/run-after.sh
+docker compose -f "$SCRIPT_DIR/docker-compose-before.yml" up &
+
+sleep 100
+
+echo "terminating containers"
+
+docker container stop $(docker ps -aq)
+
+sleep 3
+
+echo "terminated containers"
