44ecaf6db3757f07d2d1daa513eb588967b4b03f
Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>
2024-12-07 17:20:23 +0900

    add sleep between before and after scenario
    Signed-off-by: Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>

--- docker/run.sh
+++ docker/run.sh
@@ -7,4 +7,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Use the script directory to make the docker-compose.yml path relative
 $SCRIPT_DIR/run-before.sh
+
+sleep 5
+
 $SCRIPT_DIR/run-after.sh
