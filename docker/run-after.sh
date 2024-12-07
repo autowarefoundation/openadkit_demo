241cab47a43ab71f638f1dbce8ea95042d96b122
Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>
2024-12-07 19:47:41 +0900

    update script
    Signed-off-by: Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>

--- docker/run-after.sh
+++ docker/run-after.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Use the script directory to make the docker-compose.yml path relative
 docker compose -f "$SCRIPT_DIR/docker-compose-after.yml" up &
 
-sleep 100
+sleep 70
 
 echo "terminating containers"
 
