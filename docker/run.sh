da6a8c855b952f820925181e716edb924611de0c
Oguz Ozturk <oguzkaganozt@gmail.com>
2024-12-16 18:49:10 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- docker/run.sh
+++ docker/run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-
-#xhost +
-
-# Get the directory of the current script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+CONF_FILE_PASS=$SCRIPT_DIR/etc/simulation/config/pass_static_obstacle_avoidance.param.yaml
+CONF_FILE_FAIL=$SCRIPT_DIR/etc/simulation/config/fail_static_obstacle_avoidance.param.yaml
+COMMON_FILE=$SCRIPT_DIR/etc/simulation/config/common.param.yaml
 source ./simulator.env
-TIMEOUT=60 CONF_FILE=$CONF_FILE_PASS docker compose -f "$SCRIPT_DIR/docker-compose.yml" up web-visualizer -d
-# Use the script directory to make the docker-compose.yml path relative
 
+# Start visualizer
+TIMEOUT=60 COMMON_FILE=$COMMON_FILE CONF_FILE=$CONF_FILE_PASS docker compose -f "$SCRIPT_DIR/docker-compose.yml" up web-visualizer -d
+
+# Start planning-control and simulator
 while true; do
-    	echo "Updating planning container.."
-        echo "Running planning v1.."
-	TIMEOUT=70 CONF_FILE=$CONF_FILE_FAIL docker compose -f "$SCRIPT_DIR/docker-compose.yml" up planning-control simulator --abort-on-container-exit
-        echo "Updating planning container.."
-        echo "Running planning v2.."
-	TIMEOUT=120 CONF_FILE=$CONF_FILE_PASS docker compose -f "$SCRIPT_DIR/docker-compose.yml" up planning-control simulator --abort-on-container-exit
+    echo "Updating planning container.."
+    echo "Running planning v1.."
+    TIMEOUT=70 COMMON_FILE=$COMMON_FILE CONF_FILE=$CONF_FILE_FAIL docker compose -f "$SCRIPT_DIR/docker-compose.yml" up planning-control simulator --abort-on-container-exit
+    echo "Updating planning container.."
+    echo "Running planning v2.."
+    TIMEOUT=120 COMMON_FILE=$COMMON_FILE CONF_FILE=$CONF_FILE_PASS docker compose -f "$SCRIPT_DIR/docker-compose.yml" up planning-control simulator --abort-on-container-exit
 done
